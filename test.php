a30e6c422daea422bf06fb4c876d32b021f631d9
superxnero <110180629+SUPERXNERO@users.noreply.github.com>
2025-02-23 11:08:54 +0300

    update test.php

--- test.php
+++ test.php
@@ -1,7 +1,9 @@
 <?php
     date_default_timezone_set('Asia/Riyadh'); // تحديد المنطقة الزمنية
     $current_time = date('H:i:s'); // الحصول على الوقت الحالي
-?><!DOCTYPE html><html lang="ar">
+?>
+<!DOCTYPE html>
+<html lang="ar">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
